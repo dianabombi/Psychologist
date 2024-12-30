30bc2ab737194442f75880716ba1a845f396fcba
Diana <dianabombi@gmail.com>
2024-12-30 17:55:14 +0200

    about page

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 function About() {
   return (
-    <div>
-      <p>Welcome to [Psychologist's Name or Practice Name]
+    <div className="about-page">
+      <p className="text-about">Welcome to [Psychologist's Name or Practice Name]
 Your journey to well-being starts here.
 
 Are you feeling overwhelmed, stuck, or simply in need of someone to talk to? At [Practice Name], we provide a safe and supportive space where you can explore your thoughts, feelings, and challenges without judgment. Together, we’ll work toward clarity, healing, and growth.</p>
-    </div>
+    <img 
+      src="BaraPsychologist.jpg" 
+      alt="Psychologist" 
+      style={{ width: '300px', height: 'auto' }}
+      className="image-psychologist"
+      />
+
+   </div>
   )
 }
 
