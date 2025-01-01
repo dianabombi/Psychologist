fef1bbd932c0e9f2c6954eaaa5597f9b38bc3aba
Diana <dianabombi@gmail.com>
2025-01-02 01:00:20 +0200

    about me

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -12,15 +12,16 @@ function About() {
       />
 
       < NavBar />
+
 <div className="container-text">
       <p className="text-about">I am Barbora, psychologist with 6 years of practice.
         Your journey to well-being starts here.
 
         Are you feeling overwhelmed, stuck, or simply in need of someone to talk to? At Safe Space Psychology, we provide supportive space where you can explore your thoughts, feelings, and challenges without judgment. Together, we’ll work toward clarity, healing, and growth.  
       </p>
-
+        <button className='contactMe-button'>WHERE TO START ?</button>
         <button className='contactMe-button'>CONTACT ME</button>
-        <button className='contactMe-button'>GIFT OTHERS</button>
+        <button className='contactMe-button'>GIFT OTHERS</button>   
   </div>
 </div>
 </div>
