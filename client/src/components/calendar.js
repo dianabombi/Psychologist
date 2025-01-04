25d2d6989bcd09e128d9feb55a8af91024dffd45
Diana <dianabombi@gmail.com>
2025-01-04 15:55:07 +0200

    calendar final

--- client/src/components/calendar.js
+++ client/src/components/calendar.js
@@ -3,6 +3,9 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 
 const Calendar = ({ sessions }) => {
     const events = sessions.map(session => ({
@@ -11,7 +14,18 @@ const Calendar = ({ sessions }) => {
         end: new Date(new Date(session.date).getTime() + session.duration * 60000), // Calculate the end time
     }));
 
-    return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" events={events} />;
+    return <FullCalendar 
+        className="full-calendar"
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]} 
+        initialView={"dayGridMonth"} 
+        events={events} 
+        headerToolbar={{
+            start: 'today prev,next', 
+            center: 'title',
+            end: 'dayGridMonth,timeGridWeek,timeGridDay' 
+          }}
+          height={"90vh"}
+          />;
 };
 
 export default Calendar;
